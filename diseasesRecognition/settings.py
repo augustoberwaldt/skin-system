3fa48ef6407ba0080579541ab97a997c55586fb1
AugustoBerwaldt <augusto.berwaldt@gmail.com>
2017-07-30 22:25:57 -0300

    FEA:Adiciona swagger API

--- diseasesRecognition/settings.py
+++ diseasesRecognition/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework_swagger',
 ]
 
 MIDDLEWARE = [
--- diseasesRecognition/urls.py
+++ diseasesRecognition/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from rest_framework_swagger.views import get_swagger_view
+
+schema_view = get_swagger_view(title='API')
+
 urlpatterns = [
-    url(r'^polls/', include('polls.urls')),
+    url(r'^$', schema_view),
     url(r'^admin/', admin.site.urls),
 ]
