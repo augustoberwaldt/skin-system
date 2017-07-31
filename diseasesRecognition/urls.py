beb9338a37497d260ad60aafdd349bb444d7c899
augustoberwaldt <augusto.marlon@moovin.com.br>
2017-07-31 12:25:15 -0300

    FIX:debug

--- diseasesRecognition/urls.py
+++ diseasesRecognition/urls.py
@@ -18,11 +18,9 @@ from django.contrib import admin
 
 from rest_framework_swagger.views import get_swagger_view
 
-from . import views
-
 schema_view = get_swagger_view(title='API')
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', schema_view),
     url(r'^admin/', admin.site.urls),
 ]
