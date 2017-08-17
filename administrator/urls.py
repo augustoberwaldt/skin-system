5143bdd755c65bbf18d1ad2c447bab8d94bd79f0
augustoberwaldt <augusto.marlon@moovin.com.br>
2017-08-17 18:06:27 -0300

    FEA: Adiciona protoipo de tela de  login

--- administrator/urls.py
+++ administrator/urls.py
@@ -1,9 +1,8 @@
 from django.conf.urls import url, include
 from . import views;
 
-
 urlpatterns = [
-    url(r'^$', views.home , name='index'),
-    url(r'^login/', views.login , name='login'),
+    url(r'^$', views.login, name='login'),
+    url(r'^home/', views.home , name='home'),
 ]
 
