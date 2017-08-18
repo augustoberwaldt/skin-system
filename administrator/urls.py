5b3880a96d7ede2f01f36125541d4f04af3630a1
augustoberwaldt <augusto.marlon@moovin.com.br>
2017-08-18 14:08:49 -0300

    FEA:Adiciona template register

--- administrator/urls.py
+++ administrator/urls.py
@@ -3,6 +3,8 @@ from . import views;
 
 urlpatterns = [
     url(r'^$', views.login, name='login'),
+    url(r'^resetPass', views.resetPass, name='resetPass'),
+    url(r'^register', views.register, name='register'),
     url(r'^home/', views.home , name='home'),
 ]
 
--- administrator/views.py
+++ administrator/views.py
@@ -2,13 +2,19 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-from django.http  import  HttpResponse
-
+from django.http import HttpResponse
 
 
 def home(request):
     return render(request, 'home.html')
 
-
 def login(request):
     return render(request, 'auth/login.html')
+
+def resetPass(request):
+    return render(request, 'auth/resetPassword.html')
+
+def register(request):
+    return render(request, 'auth/register.html')
+
+
