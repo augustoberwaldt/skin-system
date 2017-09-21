fed9a6a18708e5d280381a4873dfc037249d145d
AugustoBerwaldt <augusto.berwaldt@gmail.com>
2017-09-20 21:45:21 -0300

    FIX: Remove print

--- administrator/controller/loginController.py
+++ administrator/controller/loginController.py
@@ -11,8 +11,6 @@ def do_login(request):
 
         user = authenticate(request, username=username, password=password)
 
-        print user
-
         if user is not None :
             login(request,user)
             return redirect('/admin/home')
